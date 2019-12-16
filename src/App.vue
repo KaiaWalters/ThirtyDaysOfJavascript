a5a116e7cd209159ec69adc418fd0ae0a8bee356
KWalters12 <kwalters12@humana.com>
2019-12-16 10:00:10 -0500

    added audio method

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DrumKit msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DrumKit from './components/DrumKit.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DrumKit
   }
 }
 </script>
---
+++ src/components/DrumKit.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div v-on:click="soundOff">
+      <span>A</span>
+      <span>CLAP</span>
+    </div>
+    <div v-on:click="soundOff">
+      <span>B</span>
+      <span>HIHAT</span>
+    </div>
+    <div @click="soundOff('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3')">
+      <span>C</span>
+      <span>KICK</span>
+    </div>
+    <div v-on:click="soundOff">
+      <span>D</span>
+      <span>OPENHAT</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DrumKit',
+  props: {
+    msg: String
+  },
+  methods: {
+    soundOff(sound){
+      alert("tomato")
+      var audio = new Audio(sound);
+      audio.play();
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div{
+  border: 2px solid green;
+  padding:10px;
+}
+.container{
+  border: 2px solid black;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-size: 15px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+}
+span{
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+}
+</style>
