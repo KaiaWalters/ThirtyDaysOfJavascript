cfda0cf262a135e90ae809a59d9adc1211a714d9
KWalters12 <kwalters12@humana.com>
2019-12-18 08:34:41 -0500

    doesn't toggle selectedButton class

--- src/components/DrumKit.vue
+++ src/components/DrumKit.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="container">
-    <div v-on:click="soundOff">
+    <button
+     v-shortkey="['a']" @shortkey="(soundOff('https://www.kozco.com/tech/piano2.wav'))"
+     v-bind:class="{selectedButton}"
+     class="first"
+     >
       <span>A</span>
       <span>CLAP</span>
-    </div>
-    <div v-on:click="soundOff">
+    </button>
+    <button 
+     v-shortkey="['b']" @shortkey="soundOff('https://www.kozco.com/tech/LRMonoPhase4.wav')"
+     v-bind:class="{selectedButton}"
+     @click="removeClass"
+     class="second"
+    >
       <span>B</span>
       <span>HIHAT</span>
-    </div>
-    <div @click="soundOff('https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3')">
+    </button>
+    <button 
+    v-shortkey="['c']" @shortkey="soundOff('https://www.pacdv.com/sounds/people_sound_effects/applause-1.wav')"
+    v-bind:class="{selectedButton}"
+    class="third"
+    >
       <span>C</span>
       <span>KICK</span>
-    </div>
-    <div v-on:click="soundOff">
+    </button>
+    <button 
+    v-shortkey="['d']" @shortkey="soundOff('https://www.pacdv.com/sounds/people_sound_effects/applause-1.wav')"
+    v-bind:class="{selectedButton}"
+    class="fourth"
+    >
       <span>D</span>
       <span>OPENHAT</span>
-    </div>
+    </button>
   </div>
 </template>
 
@@ -25,20 +42,34 @@ export default {
   props: {
     msg: String
   },
-  methods: {
-    soundOff(sound){
-      alert("tomato")
-      var audio = new Audio(sound);
-      audio.play();
+data: function (){
+  return{
+    isActive: true,
+  }
+},
+computed: {
+  selectedButton: function () {
+    return {
+      active: this.isActive 
     }
   }
-}
+  },
+  methods: { 
+     soundOff(sound){
+      alert("Ham Spamwhich")
+      var audio = new Audio(sound);
+      audio.play();
+      this.set(this.isActive,!this.isAcvtive)//get vue.se t to work bc/ this is active is not updating (toggling between true and false) in Dom 
+      }
+      //after clicking a button toggling the style back
+    }   
+  }
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div{
+button{
   border: 2px solid green;
   padding:10px;
 }
@@ -49,11 +80,13 @@ div{
   justify-content: space-around;
   font-size: 15px;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-
 }
 span{
   display: flex;
   justify-content: center;
   align-items: center; 
+}
+.selectedButton{
+  border: 10px solid coral;
 }
 </style>
